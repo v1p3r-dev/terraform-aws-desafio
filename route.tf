58b4318d55af24701cb1ac812329266b928d5f24
Victor Lucas <victorlucasla@outlook.com>
2026-06-27 14:24:14 -0300

    feat(network): finaliza infraestrutura de rede

--- route.tf
+++ route.tf
@@ -1,7 +1,63 @@
+# ==========================
+# Public Route Table
+# ==========================
+
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   tags = {
     Name = "${var.project_name}-public-route-table"
   }
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+# ==========================
+# Private Route Table
+# ==========================
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-private-route-table"
+  }
+}
+
+resource "aws_route" "private_nat_access" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.main.id
+}
+
+# ==========================
+# Associations - Public
+# ==========================
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
+
+# ==========================
+# Associations - Private
+# ==========================
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private.id
 }
